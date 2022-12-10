92bba7e6ac83bf768d91a88953b7f151f4c45589
MFRC <pranavmundhe3@gmail.com>
2022-12-10 20:54:52 +0530

    master 2 tabs integrated

--- biography-service/src/main/java/com/info/biographyservice/controller/SkillController.java
+++ biography-service/src/main/java/com/info/biographyservice/controller/SkillController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins="http://localhost:4200",allowedHeaders="*")
+@CrossOrigin(origins="http://localhost:4200/",allowedHeaders="*")
 @RestController
 @RequestMapping("/skill")
 @Slf4j
